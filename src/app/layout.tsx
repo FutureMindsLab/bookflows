a24031f620e304deb04364f592575fea9d09895a
Thiago Caserta <thiagocaserta@Thiagos-MacBook-Air.local>
2024-09-28 22:35:45 -0300

    Fix  Dashboard

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './globals.css'
+import { Analytics } from "@vercel/analytics/react"
 import { SupabaseProvider } from '@/Components/supabaseProvider'
 
 // src/app/layout.tsx
